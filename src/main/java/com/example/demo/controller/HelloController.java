8019014857ed8af808fb82d883cac3a2f6c3841c
knowyrtech <111655092+knowyrtech@users.noreply.github.com>
2022-08-21 00:03:55 +0530

    sixth commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 	
 	@GetMapping
 	public String sayHello() {
-		return "The website is up and running fifth commit";
+		return "The website is up and running sixth commit";
 	}
 
 }
